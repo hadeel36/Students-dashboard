fe996689b54de125d1888ab28c4354981af6773c
Hadeel <hadylgk87@gmail.com>
2018-12-10 10:57:43 +0200

    enhance student table functionality

--- Client-side/src/app/components/students-list/students-list.component.ts
+++ Client-side/src/app/components/students-list/students-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 // Services
 import { StudentApi, StudentInterface } from 'src/sdk';
 
+const EDIT_STUDENT_URL = 'student/edit/';
 @Component({
   selector: 'app-students-list',
   templateUrl: './students-list.component.html',
@@ -16,11 +18,20 @@ export class StudentsListComponent implements OnInit {
   ];
   public studentsList: Array<StudentInterface>;
 
-  constructor(private studentApi: StudentApi) {}
+  constructor(private studentApi: StudentApi, private router: Router) {}
 
   ngOnInit() {
     this.studentApi.find().subscribe((data: any) => {
       this.studentsList = data;
     });
+  }
+
+  selectStudent(studentId) {
+    this.router.navigate([`${EDIT_STUDENT_URL}${studentId}`]);
+  }
+
+  deleteStudent(studentId) {
+    event.stopPropagation();
+    console.log(studentId);
   }
 }
